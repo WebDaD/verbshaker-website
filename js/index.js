7ee045751373923cbd4cd3eeade54f0f027a90da
Dominik Sigmund <dominik.sigmund@br.de>
2022-08-17 15:29:18 +0200

    initial structure and todos

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
+    // TODO: all is loaded, mix first verb
+  }
+};
+function createMixedVerb (language) {
+  // TODO: by language, get a random front and back from verbs
+}
+var verbs = {
+  "de": [
+    ["Der frühe Vogel", "fängt den Wurm"]
+  ]
+}
+// TODO: add verbs to verbs array and languages from database
+
+// TODO: pwa https://www.loginradius.com/blog/engineering/build-pwa-using-vanilla-javascript/
